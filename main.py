882644b350822c9384ca45309b52aca80bb546bf
doctorjuta <opora.foto@gmail.com>
2017-10-31 17:45:12 +0200

    Minor fix of main function

--- main.py
+++ main.py
@@ -9,22 +9,23 @@ for line in f2:
     line = line.replace("\n", "")
     res = ''
     try:
-        html_doc = urllib.request.urlopen('https://www.youtube.com/'+line+'/about')
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        for s in soup.find_all("span", "about-stat"):
-            t = s.find("b")
-            if t:
-                ft = t.string
-                ft = ft.strip().replace(" ", "")
-                if len(ft) > 0:
-                    res += ft + ", "
-        if len(res) > 3:
-            res = res[:-2]
-            f1.write(res+"\n")
-            print("Data for user was added: "+line)
-        else:
-            f1.write("0, 0\n")
-            print("Can not add data for next line: "+line)
+        if len(line) > 4:
+            html_doc = urllib.request.urlopen('https://www.youtube.com/'+line+'/about')
+            soup = BeautifulSoup(html_doc, 'html.parser')
+            for s in soup.find_all("span", "about-stat"):
+                t = s.find("b")
+                if t:
+                    ft = t.string
+                    ft = ft.strip().replace(" ", "")
+                    if len(ft) > 0:
+                        res += ft + ", "
+            if len(res) > 3:
+                res = res[:-2]
+                f1.write(res+"\n")
+                print("Data for user was added: "+line)
+            else:
+                f1.write("0, 0\n")
+                print("Can not add data for next line: "+line)
     except HTTPError:
         f1.write("Was removed, Was removed\n")
         print("Channel was removed.")
